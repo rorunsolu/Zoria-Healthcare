19152cd4c57b9ad36fb95dfb247f4c6e0d4b7166
Ray Orunsolu <rorunsolu@gmail.com>
2025-01-07 01:58:22 +0000

    added new comps

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,8 +28,8 @@ export default {
     },
   },
   plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
+    function ({ addComponents }) {
+      addComponents({
         ".container": {
           width: "85%",
           margin: "0 auto",
@@ -38,6 +38,11 @@ export default {
           "@media (max-width: 700px)": {
             width: "92.5%",
           },
+        },
+        ".button-wrapper": {
+          display: "flex",
+          gap: "1rem",
+          with: "100%",
         },
         ".button": {
           borderWidth: "0.0625rem",
@@ -50,6 +55,22 @@ export default {
             cursor: "pointer",
           },
         },
+        ".button--green": {
+          padding: "0.5rem 1rem",
+          backgroundColor: "var(--color-green)",
+          color: "var(--color-white)",
+          "&:hover": {
+            backgroundColor: "var(--color-green-hover)",
+          },
+        },
+        ".button--white": {
+          padding: "0.5rem 1rem",
+          backgroundColor: "var(--color-white)",
+          color: "var(--color-green)",
+          "&:hover": {
+            backgroundColor: "var(--color-white-hover)",
+          },
+        },
         ".button--header": {
           padding: "1rem 2.75rem",
           backgroundColor: "var(--color-white)",
@@ -59,19 +80,35 @@ export default {
           },
         },
         ".button--nav": {
-          padding: "0.5rem 1rem;",
+          padding: "0.5rem 1rem",
           backgroundColor: "var(--color-white)",
           color: "var(--color-green)",
           "&:hover": {
             backgroundColor: "var(--color-white-hover)",
           },
         },
+        ".absolute-icon": {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          top: "0",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "4.5rem",
+          height: "4.5rem",
+          borderRadius: "50%",
+          border: "5px solid var(--color-white)",
+          fontSize: "2.25rem",
+          color: "var(--color-white)",
+          backgroundColor: "var(--color-green)",
+        },
         ".h1": {
-          "@apply font-semibold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem]":
+          "@apply font-bold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem]":
             {},
         },
         ".h2": {
-          "@apply text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[2.5rem] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight":
+          "@apply text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[2.5rem] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight font-bold":
             {},
         },
         ".body-1": {
@@ -79,7 +116,7 @@ export default {
             {},
         },
       });
-    },
+    }
   ],
 }
 
